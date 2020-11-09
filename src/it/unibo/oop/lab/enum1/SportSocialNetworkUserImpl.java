114a97d0a96ba018820fc1175fba65d6a95b3d1d
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2020-11-09 17:10:47 +0100

    Completed class Sport and class SportSocialNetworkUserImpl

--- src/it/unibo/oop/lab/enum1/SportSocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/enum1/SportSocialNetworkUserImpl.java
@@ -3,6 +3,9 @@
  */
 package it.unibo.oop.lab.enum1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUserImpl;
 import it.unibo.oop.lab.socialnetwork.User;
 
@@ -30,7 +33,8 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      * 
      * add a field to keep track of the set of sports followed/done by a user
      */
-
+	final List <Sport> list = new ArrayList<>();
+	
     /**
      * Builds a new {@link SportSocialNetworkUserImpl}.
      * 
@@ -78,7 +82,9 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      */
     // TODO
     public void addSport(final Sport sport) {
-
+    	if (list.contains(sport)==false) {
+    		list.add(sport);
+    	}
     }
 
     /**
@@ -90,6 +96,12 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      * @return true if the user likes sport s
      */
     public boolean hasSport(final Sport s) {
-        return false;
+    	if (list.isEmpty()) {
+    		return false;
+    	}
+    	else
+    	{
+    		return true;
+    	}
     }
 }
